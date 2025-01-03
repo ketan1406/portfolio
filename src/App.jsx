8303355472a8db7e54aad83e1bd9a24b6ee9620e
Ketan <sainiketan99@gmail.com>
2025-01-03 18:11:12 +0530

    Integrated Navbar, About and Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+// src/App.jsx
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import {
   Hero,
+  Navbar,
+  About,
+  Contact,
+  StarsCanvas,
 } from "./components";
 
 const App = () => {
@@ -10,9 +14,14 @@ const App = () => {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
           <Hero />
         </div>
-
+        <About />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
   );
