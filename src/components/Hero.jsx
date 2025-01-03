9fc41f2d78a030b9f258bca7bb570b49f9b2bdb9
Ketan <sainiketan99@gmail.com>
2025-01-03 18:13:12 +0530

    toggler removed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 import Typewriter from "typewriter-effect";
