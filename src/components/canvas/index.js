86db998439f94ac8f788579c858793563c60c66e
Ketan <sainiketan99@gmail.com>
2025-01-06 21:45:54 +0530

    Added export ParticleBackground

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,3 +1,4 @@
 export { default as EarthCanvas } from "./Earth";
 export { default as ComputersCanvas } from "./Computers";
 export { default as StarsCanvas } from "./Stars";
+export { default as ParticleBackground } from "./Particles";
