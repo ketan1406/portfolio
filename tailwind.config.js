695cdd0e4e42de5ba411b37805c9e3ed0417de34
Ketan <sainiketan99@gmail.com>
2025-01-27 09:50:09 +0530

    Updated: screen widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
       },
       screens: {
         xs: "450px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
   },
