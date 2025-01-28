0966ba241c6f6d28e47f5817838d8986d215d081
Ketan <sainiketan99@gmail.com>
2025-01-28 13:54:04 +0530

    Updated: project cards with tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         card: "0 35px 120px -15px #211e35",
       },
       screens: {
-        xs: "320px",
+        xs: "250px",
         sm: "640px",
         md: "768px",
         lg: "1024px",
